ca6a302ddad82b32ddf00284847cd4be4f8d6d3e
sco837leone <64247124+sco837leone@users.noreply.github.com>
2020-05-22 15:46:15 +0900

    Add files via upload

---
+++ MyTimer/SettingViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SettingViewController.swift
+//  MyTiimer
+//
+//  Created by USER1 on 2020/05/20.
+//  Copyright © 2020 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
+    // UIPickerViewに表示するデータをArrayで作成
+    let settingArrey : [Int] = [10,20,30,40,50,60]
+    
+    //設置値を覚えるキーを設定
+    let settingKey = "timer_value"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // timerSettingPickerのデリゲートとデータソースの通知先を指定
+        timerSettingPicker.delegate = self
+        timerSettingPicker.dataSource = self
+        
+        // UserDefaultsの取得
+        let settings = UserDefaults.standard
+        let timerValue = settings.integer(forKey: settingKey)
+        
+        // Pickerの選択を合わせる
+        for row in 0..<settingArrey.count {
+            if settingArrey[row] == timerValue {
+                timerSettingPicker.selectRow(row, inComponent: 0, animated: true)
+            }
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    @IBAction func decisionButtonAction(_ sender: Any) {
+        // 前画面に戻る
+        _ = navigationController?.popViewController(animated: true)
+    }
+    // UIPickerViewの列数を設定
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewの行数を取得
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return settingArrey.count
+    }
+    
+    // UIPickerViewの表示する内容を設定
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(settingArrey[row])
+    }
+    
+    // picker選択時に実行
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // UserDefaultsの設定
+        let settings = UserDefaults.standard
+        settings.setValue(settingArrey[row], forKey: settingKey)
+        settings.synchronize()
+    }
+}
